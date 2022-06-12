1bef942b2afb4ff16aeab8c59b1173cad5fe952e
stanleyhal <37350050+stanleyhal@users.noreply.github.com>
2022-06-12 18:12:16 +0200

    Create fav2hash.py

---
+++ fav2hash.py
@@ -0,0 +1,23 @@
+import requests
+import base64
+import mmh3
+
+def fav2hash():
+	question = input ("Is your favicon located to an URL (1) or on local path (2)?: ")
+
+	if question == "1":
+		url=input ("\nInsert the url favicon: ")
+		response = requests.get(url)
+		convertedData = base64.encodebytes(response.content).decode()
+		hash = mmh3.hash(convertedData)
+		print("\nHere's your mmh3 hash. Copy/paste this string into Shodan:\nhttp.favicon.hash:", hash)
+	elif question == "2":
+		localpath=input ("\nPlease insert the local path of the favicon, with the name and the extension:\n")
+		with open(localpath, 'rb') as image_file:
+			encoded_string = base64.encodebytes(image_file.read())
+		hash = mmh3.hash(encoded_string)
+		print("\nHere's your mmh3 hash. Copy/paste this string into Shodan:\nhttp.favicon.hash:",hash)
+	elif question != "1" or "2":
+		print("Wrong choice! Please type the right one\n")
+		fav2hash()
+fav2hash()
